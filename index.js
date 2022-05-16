049efaf766f764f18afeeed75065a0eb5d41592a
BlueBird <kirk.maningat@gmail.com>
2022-05-16 21:37:13 +0900

    Created User Router

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
@@ -19,6 +21,9 @@ mongoose.connect(
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
+
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(8800, () => {
   console.log('Backend server is running!');
